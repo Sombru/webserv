e1f7b64a19d948bf39f8544f81bca2c210f2901d
Yumzhana Tsyrendorzhieva <ytsyrend@c3r6s4.42prague.com>
2025-06-24 18:57:17 +0200

    project structure change

---
+++ include/Libraries.hpp
@@ -0,0 +1,12 @@
+#ifndef LIBRARIES_HPP
+# define LIBRARIES_HPP
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+
+#endif
